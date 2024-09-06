d35552b3cecdb93cb2d02670a2fe69c860d8a9cc
KalyssonRyan <kalyssonryan@gmail.com>
2024-09-06 20:25:23 -0300

    Adicionando Classe Consulta

---
+++ src/main/java/com/mycompany/postodesaude/Consulta.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.postodesaude;
+import java.sql.Time;
+import java.util.Date;
+
+
+/**
+ *
+ * @author Kalysson
+ */
+
+
+public class Consulta {
+    private Date data;
+    private Time hora;
+}
