b5dfe86a0462ab41c61fedd89d03d58e2961fae3
KalyssonRyan <kalyssonryan@gmail.com>
2024-09-06 20:27:00 -0300

    Criando classe medico

---
+++ src/main/java/com/mycompany/postodesaude/Medico.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.postodesaude;
+
+/**
+ *
+ * @author Kalysson
+ */
+public class Medico extends Pessoa{
+     private String especialidade;
+     private String crm;
+
+     public void setEspecialidade(String especialidade){
+         this.especialidade = especialidade;
+     }
+
+     public String getEspecialidade(){
+         return especialidade;
+     }
+
+     public void setCrm(String crm){
+         this.crm = crm;
+     }
+
+     public String getCrm(){
+         return crm;
+     }
+
+     //public void emcaminharParaTratamento()
+}
