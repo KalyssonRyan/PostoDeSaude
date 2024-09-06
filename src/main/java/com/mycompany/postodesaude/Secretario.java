eeebcd4dcde55bca56412cced4a984164b213786
KalyssonRyan <kalyssonryan@gmail.com>
2024-09-06 20:28:37 -0300

    criando classe secretario

---
+++ src/main/java/com/mycompany/postodesaude/Secretario.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.postodesaude;
+
+/**
+ *
+ * @author Kalysson
+ */
+public class Secretario extends Pessoa{
+    private String turno;
+
+    public void setTurno(String turno){
+        this.turno = turno;
+    }
+
+    public String getTurno(){
+        return turno;
+    }
+
+
+}
