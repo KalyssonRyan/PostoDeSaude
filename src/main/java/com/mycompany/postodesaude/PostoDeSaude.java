8323f911d2440f645b4a1c560dfafc1fa36cf127
KalyssonRyan <kalyssonryan@gmail.com>
2024-09-06 21:10:48 -0300

    Realizando ajustes na main

--- src/main/java/com/mycompany/postodesaude/PostoDeSaude.java
+++ src/main/java/com/mycompany/postodesaude/PostoDeSaude.java
@@ -4,11 +4,29 @@
 
 package com.mycompany.postodesaude;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Kalysson
  */
 public class PostoDeSaude {
+	private ArrayList<Paciente> pacientes;
+
+	private ArrayList<Secretario> secretarios;
+
+	private ArrayList<Enfermeiro> enfermeiros;
+
+	private ArrayList<Consulta> consultas;
+
+	private ArrayList<Farmaceutico> farmaceuticos;
+
+	private ArrayList<Medicamento> medicamentos;
+
+	private ArrayList<Receita> receitas;
+
+	private ArrayList<Medico> medicos;
+	private ArrayList<Exame> exames;
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
