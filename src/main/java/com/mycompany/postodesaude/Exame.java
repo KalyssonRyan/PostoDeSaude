7155a9435c0175ecc81ff6da5c9a300020b9cd16
KalyssonRyan <kalyssonryan@gmail.com>
2024-09-06 21:05:48 -0300

    Criando classe Exame

---
+++ src/main/java/com/mycompany/postodesaude/Exame.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.postodesaude;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Kalysson
+ */
+public class Exame {
+
+	private String nomeExame;
+
+	private Date dataSolicitacao;
+
+	private String resultado;
+
+	private Medico medicoSolicitante;
+
+	private Date dataRealizacao;
+
+	private Paciente paciente;
+
+	private String tipoExame;
+
+	public String getNomeExame() {
+		return null;
+	}
+
+	public void setNomeExame(String nomeExame) {
+
+	}
+
+	public Date getDataSolicitacao() {
+		return null;
+	}
+
+	public void setDataSolicitacao(Date dataSolicitacao) {
+
+	}
+
+	public String getResultado() {
+		return null;
+	}
+
+	public void setResultado(String resultado) {
+
+	}
+
+	public Medico getMedicoSolicitante() {
+		return null;
+	}
+
+	public void setMedicoSolicitante(Medico medico) {
+
+	}
+
+	public Date getDataRealizacao() {
+		return null;
+	}
+
+	public void setDataRealizacao(Date dataRealizacao) {
+
+	}
+
+	public Paciente getPaciente() {
+		return null;
+	}
+
+	public void setPaciente(Paciente paciente) {
+
+	}
+
+	public String getTipoExame() {
+		return null;
+	}
+
+	public void setTipoExame(String tipoExame) {
+
+	}
+
+}
