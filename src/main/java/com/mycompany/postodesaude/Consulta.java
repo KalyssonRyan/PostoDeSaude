0bdda0c766428bc262e12c2714e6c60edc800c22
Ruan Rocha <ruanrochaeuok@gmail.com>
2024-09-09 12:54:13 -0300

    Adicionando geters, seters e construtor

--- src/main/java/com/mycompany/postodesaude/Consulta.java
+++ src/main/java/com/mycompany/postodesaude/Consulta.java
@@ -9,11 +9,55 @@ import java.util.Date;
 
 /**
  *
- * @author Kalysson
+ * @author Kalysson @author Ruan
  */
 
 
 public class Consulta {
+
+
     private Date data;
     private Time hora;
+    //setando o medico da consulta
+    private Medico medico;
+    //setando o paciente da consulta
+    private Paciente paciente;
+
+    public Consulta(Date data, Time hora, Medico medico, Paciente paciente){
+
+        this.data = data;
+        this.hora = hora;
+        this.medico = medico;
+        this.paciente = paciente;
+    }
+
+    public void setData(Date data){
+        this.data = data;
+    }
+
+    public String getData(Date data){
+        return data;
+    }
+
+    public void setHora(Time hora){
+        this.hora = hora;
+    }
+
+    public Time getHora(){
+        return hora;
+    }
+
+    public void setMedico(Medico medico){
+        this.medico = medico;
+    }
+
+    public Medico getMedico(){
+        return medico;
+    }
+
+    public void setPaciente(Paciente paciente){
+        this.paciente = paciente;
+    }
+
+
 }
